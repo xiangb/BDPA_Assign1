8fa7e6df88d640160a1c2f7458d6d8ca33531fdd
xiangb <xiangb@github.com>
2017-02-13 14:43:06 -0800

    [ADD] eclipse project for stopwords 10red with combi, 10red with combi comp, 50 red with combi comp

---
+++ Stopwords_10red_comb_compr/src/BDPA/Findstopwords_10red_comb_compr.java
@@ -0,0 +1,102 @@
+package BDPA;
+
+import java.io.IOException;
+import java.util.Arrays;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+
+
+public class Findstopwords_10red_comb_compr extends Configured implements Tool {
+   public static void main(String[] args) throws Exception {
+      System.out.println(Arrays.toString(args));
+      int res = ToolRunner.run(new Configuration(), new Findstopwords_10red_comb_compr(), args);
+      
+      System.exit(res);
+   }
+
+   @Override
+   public int run(String[] args) throws Exception {
+      System.out.println(Arrays.toString(args));
+      Job job = new Job(getConf(), "Findstopwords_10red_comb_compr");
+      job.setJarByClass(Findstopwords_10red_comb_compr.class);
+      job.setOutputKeyClass(Text.class);
+      job.setOutputValueClass(IntWritable.class);
+
+      job.setMapperClass(Map.class);
+      job.setReducerClass(Reduce.class);
+      
+      /* set number of reducers */
+      job.setNumReduceTasks(10);
+      
+      /* set combiner */
+      job.setCombinerClass(Reduce.class);
+      
+      /* set intermediate compression codec for mapper output */
+      
+      job.getConfiguration().set("mapreduce.compress.map.output", "true");
+      job.getConfiguration().set("mapreduce.output.compression.type", "BLOCK"); 
+      job.getConfiguration().set("mapreduce.map.output.compression.codec", "org.apache.hadoop.io.compress.GzipCodec"); 
+
+      job.setInputFormatClass(TextInputFormat.class);
+      job.setOutputFormatClass(TextOutputFormat.class);
+      
+      /* set output as csv file */
+      job.getConfiguration().set("mapreduce.output.textoutputformat.separator", ",");
+      
+      
+      FileInputFormat.addInputPath(job, new Path(args[0]));
+      FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+      job.waitForCompletion(true);
+      
+      return 0;
+   }
+   
+   public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+      private final static IntWritable ONE = new IntWritable(1);
+      private Text word = new Text();
+
+      @Override
+      public void map(LongWritable key, Text value, Context context)
+              throws IOException, InterruptedException {
+         for (String token: value.toString().replaceAll("[^a-zA-Z ]", "").split("\\s+")) {
+
+        	 	
+        	 	word.set(token.toLowerCase());
+                context.write(word, ONE);
+                
+        	 	
+        
+
+         }
+      }
+   }
+
+   public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+      @Override
+      public void reduce(Text key, Iterable<IntWritable> values, Context context)
+              throws IOException, InterruptedException {
+         int sum = 0;
+         for (IntWritable val : values) {
+            sum += val.get();
+         }
+         if (sum > 4000){
+         context.write(key, new IntWritable(sum));
+         }
+      }
+   }
+}
